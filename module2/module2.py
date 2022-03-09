de26d37724bd950a51a2932726f1019fc10dcb48
marhotiewer <marhotiewer@protonmail.com>
2022-03-09 12:11:59 +0100

    finished module 2

--- module2/module2.py
+++ module2/module2.py
@@ -5,6 +5,7 @@ from skimage import feature
 
 guassian = [[1/16,1/8,1/16],[1/8,1/4,1/8],[1/16,1/8,1/16]]
 laplacian = [[0.5,1,0.5],[1,-6,1],[0.5,1,0.5]]
+edge = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
 
 src3d = pyplot.imread("sample.jpg")
 src2d = src3d[:,:,0]
@@ -15,27 +16,27 @@ window.add_subplot(331)
 pyplot.imshow(src3d, cmap='gray')
 
 img = convolve(src2d, guassian)
-window.add_subplot(332)
-pyplot.imshow(img, cmap='gray')
-
-img = convolve(img, laplacian)
-window.add_subplot(333)
-pyplot.imshow(img, cmap='gray')
-
-img = filters.prewitt(src3d)
 window.add_subplot(334)
 pyplot.imshow(img, cmap='gray')
 
-img = filters.scharr(src3d)
+img = convolve(img, laplacian)
 window.add_subplot(335)
 pyplot.imshow(img, cmap='gray')
 
-img = filters.sobel(src3d)
+img = convolve(src2d, edge)
 window.add_subplot(336)
 pyplot.imshow(img, cmap='gray')
 
-img = feature.canny(src2d)
+img = filters.scharr(src3d)
+window.add_subplot(337)
+pyplot.imshow(img, cmap='gray')
+
+img = filters.sobel(src3d)
 window.add_subplot(338)
+pyplot.imshow(img, cmap='gray')
+
+img = feature.canny(src2d)
+window.add_subplot(339)
 pyplot.imshow(img, cmap='gray')
 
 pyplot.show()
